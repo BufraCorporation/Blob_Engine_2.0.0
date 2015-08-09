ecc1277c9312cf8447e9220ca53db0e9f244e35d
Marc <burianmarc@googlemail.com>
2015-08-09 23:09:28 +0200

    test

--- core/src/de/bufracorporation/blob_engine/MyGdxGame.java
+++ core/src/de/bufracorporation/blob_engine/MyGdxGame.java
@@ -1,27 +1,69 @@
 package de.bufracorporation.blob_engine;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.Timer;
 
-public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+/**
+ * Created by Marc on 08.08.2015.
+ */
+public class MyGdxGame extends Game{
+
+    private SpriteBatch batch;
+    private Skin skin;
+    private TextButton txtButton;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+
+        skin = new Skin(Gdx.files.internal("json/def.json"));
+        txtButton = new TextButton("Spielen", skin);
+        txtButton.setBounds(200, 200, 100, 30);
+
+    }
+
+
+    @Override
+    public void render() {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClearColor(0.4f, 0.6f, 0.3f, 1);
+
+        batch.begin();
+
+        txtButton.draw(batch, 1);
+
+        batch.end();
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
 }
